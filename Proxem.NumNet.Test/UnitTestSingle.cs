b7ba5bc4fe615e7eda03b433c97b52503247cd37
DC3\thomas.perrais <thp@proxem.com>
2019-04-23 15:14:58 +0200

    update BlasNet version and csproj

--- Proxem.NumNet.Test/UnitTestSingle.cs
+++ Proxem.NumNet.Test/UnitTestSingle.cs
@@ -175,6 +175,56 @@ namespace Proxem.NumNet.Test
         }
 
         [TestMethod]
+        public void TestAddSubScalar()
+        {
+            var a = NN.Array(new float[,] {
+                { 1, -1, 2, 3 },
+                { 1, 0, 0, -4 },
+                { 2, -1, 6, -1 }
+            });
+            var b = -1;
+            var c = 2;
+            var d = -3;
+            var apb = NN.Array(new float[,] {
+                { 0, -2, 1, 2 },
+                { 0, -1, -1, -5 },
+                { 1, -2, 5, -2 }
+            });
+            var apc = NN.Array(new float[,] {
+                { 3, 1, 4, 5 },
+                { 3, 2, 2, -2 },
+                { 4, 1, 8, 1 }
+            });
+            var amd = NN.Array(new float[,] {
+                { 4, 2, 5, 6 },
+                { 4, 3, 3, -1 },
+                { 5, 2, 9, 2 }
+            });
+            var amc = NN.Array(new float[,] {
+                { -1, -3, 0, 1 },
+                { -1, -2, -2, -6 },
+                { 0, -3, 4, -3 }
+            });
+            var apbmd = NN.Array(new float[,] {
+                { 3, 1, 4, 5 },
+                { 3, 2, 2, -2 },
+                { 4, 1, 8, 1 }
+            });
+
+            AssertArray.AreAlmostEqual(a + b, apb);
+            AssertArray.AreAlmostEqual(a.Add(b), apb);
+            AssertArray.AreAlmostEqual(a - d, amd);
+            AssertArray.AreAlmostEqual(a.Sub(d), amd);
+            AssertArray.AreAlmostEqual(a + c, apc);
+            AssertArray.AreAlmostEqual(a.Add(c), apc);
+            AssertArray.AreAlmostEqual(a - c, amc);
+            AssertArray.AreAlmostEqual(a.Sub(c), amc);
+
+            AssertArray.AreAlmostEqual(a.Add(b, result: a), apb);
+            AssertArray.AreAlmostEqual(a.Sub(d, result: a), apbmd);
+        }
+
+        [TestMethod]
         public void TestAddMat()
         {
             var a = NN.Array(new float[,] {
